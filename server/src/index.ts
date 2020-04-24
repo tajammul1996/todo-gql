7a0fe1a062275bdeb09125a5063eb9d27e074704
tajammul1996 <tajammul.pasha@thoughtworks.com>
2020-04-24 07:34:51 +0530

    basic setup done

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,4 +2,6 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => res.send("Hello world"));
+
 app.listen(4000, () => console.log(`[server started]: [port 4000]`));
